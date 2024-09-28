6e905929740c4756b1f7513509064bda817952af
Vinisha Manek <Vinisha@exoshop.com>
2024-09-28 19:37:10 -0400

    fixed for kesh (sorry)

---
+++ src/frontend/components/player.js
@@ -0,0 +1,35 @@
+class Player {
+    constructor(x, y, width, height, name) {
+        this.health = 5; 
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.name = name;
+        this.isColliding = false; 
+    }
+
+    draw() {
+        ctx.fillStyle = this.isColliding ? 'orange' : 'green'; 
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+
+    update(mouseX, mouseY) {
+        this.x = mouseX - this.width / 2;
+        this.y = mouseY - this.height / 2;
+        this.draw();
+    }
+
+    loseHealth() {
+        this.health = Math.max(0, this.health - 1); 
+        console.log(`${this.name} lost health! Current health: ${this.health}`);
+        if (this.health === 0) {
+            endGame(); 
+        }
+    }
+
+    gainHealth(){
+        this.health = Math.min(5, this.health + 1); 
+        console.log(`${this.name} gained health! Current health: ${this.health}`);
+    }
+}
