c64b1bbe3dae79d6753e5a8a8c90e75aa88a3c1c
katkes <keshank1@hotmail.com>
2024-09-28 01:31:56 -0400

    Testing video capture
    Should have video

--- src/app.py
+++ src/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from picamera import PiCamera
+import io
+import time
 
 app = Flask(__name__)
+camera = PiCamera()
 
 @app.route('/')
 def index():
-    return 'Test CI/CD'
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    stream = io.BytesIO()
+    camera.capture(stream, 'jpeg')
+    stream.seek(0)
+    return stream.read()
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
