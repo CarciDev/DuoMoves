5265cf3727456b121256054ef6b77359a2b7e303
mahutt <tommy.mahut@gmail.com>
2024-09-29 01:07:13 -0400

    Fix collisions

--- src/frontend/components/player.js
+++ src/frontend/components/player.js
@@ -1,51 +1,74 @@
-import { endGame } from "../script.js"
+import { endGame } from '../script.js'
 
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('myCanvas')
+const ctx = canvas.getContext('2d')
+
+class BodyPart {
+  static size = 50
+  constructor({ x, y }) {
+    this.x = x - BodyPart.size / 2
+    this.y = y - BodyPart.size / 2
+    this.width = BodyPart.size
+    this.height = BodyPart.size
+  }
+}
 
 export class Player {
+  constructor(id) {
+    this.health = 5
+    this.nose = null
+    this.leftWrist = null
+    this.rightWrist = null
+    this.isColliding = false
+    this.width = 50
+    this.height = 50
+    this.id = id
+  }
 
-    constructor(){
-        this.health = 5; 
-        this.nose = null;
-        this.leftWrist = null;
-        this.rightWrist = null;
-        this.isColliding = false;
-        this.width = 50;
-        this.height = 50;
+  draw() {
+    ctx.fillStyle = 'blue'
+    if (this.leftWrist) {
+      ctx.fillRect(this.leftWrist.x, this.leftWrist.y, this.width, this.height)
     }
+    if (this.rightWrist) {
+      ctx.fillRect(
+        this.rightWrist.x,
+        this.rightWrist.y,
+        this.width,
+        this.height
+      )
+    }
+    if (this.nose) {
+      ctx.fillStyle = this.isColliding ? 'orange' : 'blue'
+      ctx.fillRect(this.nose.x, this.nose.y, this.width, this.height)
+    }
+  }
 
-    draw() {
-        ctx.fillStyle = 'blue';
-        if (this.leftWrist) {
-            ctx.fillRect(this.leftWrist.x, this.leftWrist.y, this.width, this.height);
-        }
-        if (this.rightWrist) {
-            ctx.fillRect(this.rightWrist.x, this.rightWrist.y, this.width, this.height);
-        }
-        if (this.nose) {
-            ctx.fillStyle = this.isColliding ? 'orange' : 'blue'; 
-            ctx.fillRect(this.nose.x, this.nose.y, this.width, this.height);
-        }
+  update({ nose = null, left_wrist = null, right_wrist = null }) {
+    if (nose) {
+      this.nose = new BodyPart(nose)
     }
-    
-    update({nose = null, left_wrist = null, right_wrist = null}) {
-        this.nose = nose;
-        this.leftWrist = left_wrist;
-        this.rightWrist = right_wrist;
-        this.draw();
+    if (left_wrist) {
+      this.leftWrist = new BodyPart(left_wrist)
     }
+    if (right_wrist) {
+      this.rightWrist = new BodyPart(right_wrist)
+    }
+    this.draw()
+  }
 
-    loseHealth() {
-        this.health = Math.max(0, this.health - 1); 
-        console.log(`${this.name} lost health! Current health: ${this.health}`);
-        if (this.health === 0) {
-            endGame(); 
-        }
+  loseHealth() {
+    this.health = Math.max(0, this.health - 1)
+    document.querySelector(`.health-${this.id}`).style.width = `${
+      this.health * 20
+    }%`
+    if (this.health === 0) {
+      endGame()
     }
+  }
 
-    gainHealth(){
-        this.health = Math.min(5, this.health + 1); 
-        console.log(`${this.name} gained health! Current health: ${this.health}`);
-    }
+  gainHealth() {
+    this.health = Math.min(5, this.health + 1)
+    console.log(`${this.name} gained health! Current health: ${this.health}`)
+  }
 }
--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,241 +1,267 @@
-import { MovingObject } from './components/movingObjects.js';
-import {Player} from './components/player.js'
+import { MovingObject } from './components/movingObjects.js'
+import { Player } from './components/player.js'
 
-const startGameButton = document.getElementById('startGameButton');
-const playAgainButton = document.getElementById('playAgainButton');
-const exitGameButton = document.getElementById('exitGameButton');
-const startScreen = document.getElementById('startScreen');
-const canvas = document.getElementById('myCanvas');
-const ctx = canvas.getContext("2d");
-const gameHeader = document.querySelector('header');
-const timerDisplay = document.getElementById('timer');
-const gameOverMessage = document.getElementById('gameOverMessage');
-const winnerDisplay = document.getElementById('winner');
+const startGameButton = document.getElementById('startGameButton')
+const playAgainButton = document.getElementById('playAgainButton')
+const exitGameButton = document.getElementById('exitGameButton')
+const startScreen = document.getElementById('startScreen')
+const canvas = document.getElementById('myCanvas')
+const ctx = canvas.getContext('2d')
+const gameHeader = document.querySelector('header')
+const timerDisplay = document.getElementById('timer')
+const gameOverMessage = document.getElementById('gameOverMessage')
+const winnerDisplay = document.getElementById('winner')
 
-const countdownDisplay = document.createElement('div');
-countdownDisplay.style.position = 'absolute';
-countdownDisplay.style.top = '50%';
-countdownDisplay.style.left = '50%';
-countdownDisplay.style.transform = 'translate(-50%, -50%)';
-countdownDisplay.style.fontSize = '48px';
-countdownDisplay.style.color = 'white';
-countdownDisplay.style.display = 'none';
-document.body.appendChild(countdownDisplay);
+const countdownDisplay = document.createElement('div')
+countdownDisplay.style.position = 'absolute'
+countdownDisplay.style.top = '50%'
+countdownDisplay.style.left = '50%'
+countdownDisplay.style.transform = 'translate(-50%, -50%)'
+countdownDisplay.style.fontSize = '48px'
+countdownDisplay.style.color = 'white'
+countdownDisplay.style.display = 'none'
+document.body.appendChild(countdownDisplay)
 
-let p1Health = 5; 
-let p2Health = 5; 
-let timeLeft = 60;
-let gameInterval;
-let countdownInterval;
+let p1Health = 5
+let p2Health = 5
+let timeLeft = 60
+let gameInterval
+let countdownInterval
 
 function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
 }
-resizeCanvas();
-window.addEventListener('resize', resizeCanvas);
+resizeCanvas()
+window.addEventListener('resize', resizeCanvas)
 
 // start a game when button is clicked
 startGameButton.addEventListener('click', () => {
-    gameHeader.style.visibility = '';
-    startScreen.style.display = 'none'; 
-    canvas.style.display = 'block'; 
-    countdownInterval = setInterval(updateTimer, 1000); 
-    animate();
-});
+  gameHeader.style.visibility = ''
+  startScreen.style.display = 'none'
+  canvas.style.display = 'block'
+  countdownInterval = setInterval(updateTimer, 1000)
+  animate()
+})
 
 function updateTimer() {
-    timeLeft--;
-    timerDisplay.innerText = 'TIME: ' + timeLeft;
+  timeLeft--
+  timerDisplay.innerText = 'TIME: ' + timeLeft
 
-    if (timeLeft <= 0) {
-        clearInterval(countdownInterval); // countdown stops
-        endGame(time);
-    }
+  if (timeLeft <= 0) {
+    clearInterval(countdownInterval) // countdown stops
+    endGame(time)
+  }
 }
 
 export function endGame(reason) {
-    cancelAnimationFrame(gameInterval);
-    
-    timerDisplay.style.display = 'none'; 
+  cancelAnimationFrame(gameInterval)
 
-    document.getElementById('gameControls').style.display = 'block';
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'black'; 
-    ctx.fillRect(0, 0, canvas.width, canvas.height); 
+  timerDisplay.style.display = 'none'
 
-        // if (reason === 'time') {
-        //     winnerDisplay.innerText = 'TIME UP!';
-        // } else {
-        //     if (p1Health > p2Health) {
-        //         winnerDisplay.innerText = 'PLAYER 1 WINS.';
-        //     } else if (p2Health > p1Health) {
-        //         winnerDisplay.innerText = 'PLAYER 2 WINS.';
-        //     } else {
-        //         winnerDisplay.innerText = 'DRAW';
-        //     }
-        // }
-    
+  document.getElementById('gameControls').style.display = 'block'
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    // setTimeout(() => {
-    //     window.location.href = 'index.html';
-    // }, 5000);
+  // if (reason === 'time') {
+  //     winnerDisplay.innerText = 'TIME UP!';
+  // } else {
+  //     if (p1Health > p2Health) {
+  //         winnerDisplay.innerText = 'PLAYER 1 WINS.';
+  //     } else if (p2Health > p1Health) {
+  //         winnerDisplay.innerText = 'PLAYER 2 WINS.';
+  //     } else {
+  //         winnerDisplay.innerText = 'DRAW';
+  //     }
+  // }
+
+  // setTimeout(() => {
+  //     window.location.href = 'index.html';
+  // }, 5000);
 }
 
 // Collision detection function
 function detectCollision(obj1, obj2) {
-    return (
-        obj1.x < obj2.x + obj2.width &&
-        obj1.x + obj1.width > obj2.x &&
-        obj1.y < obj2.y + obj2.height &&
-        obj1.y + obj1.height > obj2.y
-    );
+  if (obj1 === null || obj2 === null) {
+    return false
+  }
+  return (
+    obj1.x < obj2.x + obj2.width &&
+    obj1.x + obj1.width > obj2.x &&
+    obj1.y < obj2.y + obj2.height &&
+    obj1.y + obj1.height > obj2.y
+  )
 }
 
 // creating moving objects
-let movingObjects = [];
+let movingObjects = []
 
 function createMovingObjects() {
-    const movementTypes = ['straight', 'angle', 'parabola'];
+  const movementTypes = ['straight', 'angle', 'parabola']
 
-    // Bad guys
-    for (let i = 0; i < 12; i++) {
-        const x = Math.random() * (canvas.width - 150);
-        const y = Math.random() * (canvas.height - 50);
-        const width = 50;
-        const height = 50;
-        const dx = Math.random() * 2 - 1;
-        const dy = 2; 
-        const color = "red"; 
-        const isEnemy = true;
-        const movementType = movementTypes[Math.floor(Math.random() * movementTypes.length)];
-        movingObjects.push(new MovingObject(x, y, width, height, dx, dy, color, isEnemy, movementType));
-    }
+  // Bad guys
+  for (let i = 0; i < 12; i++) {
+    const x = Math.random() * (canvas.width - 150)
+    const y = Math.random() * (canvas.height - 50)
+    const width = 50
+    const height = 50
+    const dx = Math.random() * 2 - 1
+    const dy = 2
+    const color = 'red'
+    const isEnemy = true
+    const movementType =
+      movementTypes[Math.floor(Math.random() * movementTypes.length)]
+    movingObjects.push(
+      new MovingObject(
+        x,
+        y,
+        width,
+        height,
+        dx,
+        dy,
+        color,
+        isEnemy,
+        movementType
+      )
+    )
+  }
 
-    // Good guys
-    for (let i = 0; i < 3; i++){
-        const x = Math.random() * (canvas.width - 150);
-        const y = Math.random() * (canvas.height - 50);
-        const width = 50;
-        const height = 50;
-        const dx = Math.random() * 2 - 1; // Random dx for angle movement
-        const dy = 2; 
-        const color = "green"; 
-        const isEnemy = false;
-        const movementType = movementTypes[Math.floor(Math.random() * movementTypes.length)];
-        movingObjects.push(new MovingObject(x, y, width, height, dx, dy, color, isEnemy, movementType));
-    }
+  // Good guys
+  for (let i = 0; i < 3; i++) {
+    const x = Math.random() * (canvas.width - 150)
+    const y = Math.random() * (canvas.height - 50)
+    const width = 50
+    const height = 50
+    const dx = Math.random() * 2 - 1 // Random dx for angle movement
+    const dy = 2
+    const color = 'green'
+    const isEnemy = false
+    const movementType =
+      movementTypes[Math.floor(Math.random() * movementTypes.length)]
+    movingObjects.push(
+      new MovingObject(
+        x,
+        y,
+        width,
+        height,
+        dx,
+        dy,
+        color,
+        isEnemy,
+        movementType
+      )
+    )
+  }
 }
 
-const player1 = new Player();
-const player2 = new Player();
-let mouseX = 1;
-let mouseY = 1;
+const player1 = new Player(1)
+const player2 = new Player(2)
+let mouseX = 1
+let mouseY = 1
 
 window.addEventListener('mousemove', (event) => {
-    const rect = canvas.getBoundingClientRect();
-    mouseX = event.clientX - rect.left;
-    mouseY = event.clientY - rect.top;
-});
+  const rect = canvas.getBoundingClientRect()
+  const scaleX = canvas.width / rect.width
+  const scaleY = canvas.height / rect.height
+  mouseX = (event.clientX - rect.left) * scaleX
+  mouseY = (event.clientY - rect.top) * scaleY
+})
 
-createMovingObjects();
+createMovingObjects()
 
-let isPaused = true;
-
+let isPaused = true
 
 function animate() {
-    // if two detections, unpause, else, pause
+  // if two detections, unpause, else, pause
 
-    if (detections === null || detections.length === 2) {
-        isPaused = false;
+  if (detections === null || detections.length === 2) {
+    isPaused = false
+  } else {
+    isPaused = true
+  }
+
+  if (isPaused) {
+    gameInterval = requestAnimationFrame(animate)
+    return
+  }
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+  try {
+    const [detectionA, detectionB] = detections
+
+    const playerAX = detectionA['nose'].x
+    const playerBX = detectionB['nose'].x
+
+    if (playerAX < playerBX) {
+      // player 1 is on the left side (smaller x-coordinate)
+      player1.update(detectionA)
+      player2.update(detectionB)
     } else {
-        isPaused = true;
+      player1.update(detectionB)
+      player2.update(detectionA)
     }
-    
-    if (isPaused) {
-        gameInterval = requestAnimationFrame(animate);
-        return;
-    }
+  } catch (e) {
+    player1.update({ nose: { x: mouseX, y: mouseY } })
+  }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  try {
+    let player1Detection = detections[0]
+    player1.update(player1Detection)
+    player2.update(player1Detection)
+  } catch (e) {
+    console.log('no detections')
+  }
 
-    try {
-       
-            const [detectionA, detectionB] = detections;
-
-            const playerAX = detectionA['nose'].x;
-            const playerBX = detectionB['nose'].x;
-
-            if (playerAX < playerBX) {
-                // player 1 is on the left side (smaller x-coordinate)
-                player1.update(detectionA)
-                player2.update(detectionB)
-            } else {
-                player1.update(detectionB)
-                player2.update(detectionA)
-            }
-    } catch (e) {
-        player1.update({ nose: { x: mouseX, y: mouseY } })
-    }
-
-    try {
-        let player1Detection = detections[0]
-        player1.update(player1Detection);
-        player2.update(player1Detection);
-    } catch (e) {
-        console.log("no detections")
-    }
-    
-
-    movingObjects.forEach((obj) => {
-        obj.update();
-        if (detectCollision(obj, player1)) {
-            if (obj.isEnemy){
-                obj.isColliding = true;
-                player1.isColliding = true;
-                player1.loseHealth(obj);
-            }
-            else {
-                player1.isColliding = true;
-                player1.gainHealth();
-            }
-            obj.isDeleted = true;
+  movingObjects.forEach((obj) => {
+    obj.update()
+    for (let player of [player1, player2]) {
+      if (detectCollision(obj, player.nose)) {
+        if (obj.isEnemy) {
+          obj.isColliding = true
+          player.isColliding = true
+          player.loseHealth(obj)
         } else {
-            obj.isColliding = false;
-            player1.isColliding = false;
+          player.isColliding = true
+          player.gainHealth()
         }
-    });
+        obj.isDeleted = true
+      } else {
+        obj.isColliding = false
+        player.isColliding = false
+      }
+    }
+  })
 
-    movingObjects = movingObjects.filter(obj => !obj.isDeleted);
-    gameInterval = requestAnimationFrame(animate);
-
+  movingObjects = movingObjects.filter((obj) => !obj.isDeleted)
+  gameInterval = requestAnimationFrame(animate)
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const playAgainButton = document.getElementById('playAgainButton');
-    const exitGameButton = document.getElementById('exitGameButton');
+  const playAgainButton = document.getElementById('playAgainButton')
+  const exitGameButton = document.getElementById('exitGameButton')
 
-    playAgainButton.addEventListener('click', () => {
-        resetGame(); 
-        animate();
-    });
+  playAgainButton.addEventListener('click', () => {
+    resetGame()
+    animate()
+  })
 
-    exitGameButton.addEventListener('click', () => {
-        window.location.href = 'index.html'; 
-    });
-});
+  exitGameButton.addEventListener('click', () => {
+    window.location.href = 'index.html'
+  })
+})
 
 function resetGame() {
-    clearInterval(countdownInterval); 
-    cancelAnimationFrame(gameInterval); 
+  clearInterval(countdownInterval)
+  cancelAnimationFrame(gameInterval)
 
-    p1Health = 5; 
-    p2Health = 5; 
-    timeLeft = 60; 
-    createMovingObjects();
+  p1Health = 5
+  p2Health = 5
+  timeLeft = 60
+  createMovingObjects()
 
-    document.getElementById('gameControls').style.display = 'none'; 
-    timerDisplay.style.display = 'block'; 
-    timerDisplay.innerText = 'TIME: ' + timeLeft; 
-
+  document.getElementById('gameControls').style.display = 'none'
+  timerDisplay.style.display = 'block'
+  timerDisplay.innerText = 'TIME: ' + timeLeft
 }
